586d08d954566664ae67da5c849783ada0cbbc9e
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-04-21 08:02:02 +0530

    Update datatype.cpp

--- datatype.cpp
+++ datatype.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    bool d;
+    
+    a =10;//intialise
+    
+    cout<<"size int" << sizeof (a) << endl;
+    cout<<"size of float"<< sizeof (b) <<endl;
+    cout<<"size char" << sizeof (c) <<endl;
+    cout<<"size of bool"<< sizeof (d)<<endl;
+
+    return 0;
+}
+
+
+op-
+size int4
+size of float4
+size char1
+size of bool1
