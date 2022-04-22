c95531bd03b951cf27f2d1239860c325877fe791
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-04-22 17:08:02 +0530

    Update 2)if_else.cpp

--- 2)if_else.cpp
+++ 2)if_else.cpp
@@ -28,3 +28,37 @@ int main()
     return 0;
 }
 
+
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int save;
+    cin>>save;
+    
+    if(save>5000)
+    {
+        cout<<"buy lv bag";
+    }
+    else if(save>2000)
+    {
+        cout<<"buy plastic bag";
+    }
+    else 
+    {
+        cout<<"dont buy anything";
+    }
+
+    return 0;
+}
