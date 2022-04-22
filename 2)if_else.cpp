710e27a2cb8a69fee051c229ad97b0035f4d5b4b
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-04-22 16:59:00 +0530

    Create 2)if_else.cpp

---
+++ 2)if_else.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int save;
+    cin>>save;
+    
+    if(save>5000)
+    {
+        cout<<"buy bag";
+    }
+    
+    else 
+    {
+        cout<<"dont buy anything";
+    }
+
+    return 0;
+}
+
